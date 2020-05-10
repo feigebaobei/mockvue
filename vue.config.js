f02f945a95a08d5749ac093089e9744fdb25e1e7
feigebaobei <18515195415@163.com>
2020-05-10 10:18:24 +0800

    添加vuecli的配置文件

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    // publicPath: '/',  //打包运行环境的根路径为/dist
+    publicPath: './',  //打包运行环境的根路径为/dist
+
+    // devServer: {  // 开发调试服务器配置项
+
+    //     open: true,  // npm run serve后自动打开页面
+
+    //     host: '0.0.0.0',  // 匹配本机IP地址
+
+    //     port: 9009,  // 开发服务器运行端口号
+
+    //     compress: true  // 启用静态资源压缩算法
+
+    // }
+
+}
