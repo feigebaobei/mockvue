854ff38fe9aed9ac1f5263016284918e08d0243f
feigebaobei <18515195415@163.com>
2020-05-10 20:21:31 +0800

    前端使用https请求ngrok里的服务

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
   // baseURL: 'http://127.0.0.1:9876/',
-  baseURL: 'http://735c3fc6.ngrok.io/',
+  baseURL: 'https://735c3fc6.ngrok.io/',
   timeout: 5000
 })
 
